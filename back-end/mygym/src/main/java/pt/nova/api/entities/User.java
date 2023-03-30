ed4f3938a8be4e7d2d3acb235d9967f8b2919e7f
Jorge-L-F <jorge.l.ferreira99@protonmail.com>
2023-03-30 17:13:45 +0100

    Added more fields to the User.

--- back-end/mygym/src/main/java/pt/nova/api/entities/User.java
+++ back-end/mygym/src/main/java/pt/nova/api/entities/User.java
@@ -3,12 +3,16 @@ package pt.nova.api.entities;
 public class User {
 
     private String userId;
+    private String name;
+    private float weight;
 
     public User(){}
 
-    public User(String userId) {
+    public User(String userId, String name, float weight) {
         super();
         this.userId = userId;
+        this.name = name;
+        this.weight = weight;
     }
 
     public String getUserId() {
@@ -18,5 +22,21 @@ public class User {
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
+    public String getUserName() {
+        return name;
+    }
+
+    public void setUserName(String name) {
+        this.name = name;
+    }
+
+    public float getUserWeight() {
+        return weight;
+    }
+
+    public void setUserWeight(float weight) {
+        this.weight = weight;
+    }
 
 }
